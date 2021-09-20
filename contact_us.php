788bef1e5e1f684f566ea4ebb1841b0f91da9e57
santhosh_antony_raj <eparis@atlassion.com>
2021-09-20 21:36:33 +0530

    first commit

---
+++ contact_us.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Contact_us</title>
+	<link rel="stylesheet" href="style_contact.css?v=<?php echo time(); ?>">
+	<link rel="stylesheet"  href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"> 
+</head>
+<body>	
+<section class="header">
+	<div class="form">
+		<form>
+		<label for="radio" class="radio">
+			<input type="radio"  id = " I agree "name="I Agree - I disagree"> I agree terms and condition policy
+			
+		</label>
+		
+	</form>
+	</div>
+	<div class="text">
+		
+	<div class="icons_c">
+		<a href="https://wa.me/919025361179"target="blank"><i class="fa fa-whatsapp"></i></a>
+		<a href="https://www.facebook.com/prabhu.dass.900"target="blank"> <i class="fa fa-facebook"></i></a>
+		<a href="https://www.instagram.com/_____.pd._____/"target="blank"><i class="fa fa-instagram"></i></a>
+		<a href="https://www.youtube.com/channel/UCsfCxIqufogUeWZ3V4sLUmQ"target="blank"><i class="fa fa-youtube"></i></a>
+		<i class="fa fa-twitter"></i>
+	<p>CLICK THE EMOTE</p>
+
+	</div>
+</div>
+</section>
+<div class="label">
+
+</div>
+
+</body>
+</html>
